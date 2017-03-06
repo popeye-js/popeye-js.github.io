2df82509f5c7a2a33e583f8e63b3143dcb1a035c
Avi kotte <kotteavin@hotmail.com>
2017-03-05 22:25:36 -0800

    add put.io lists functionality

--- js/speech.js
+++ js/speech.js
@@ -50,5 +50,31 @@
 
   annyang.addCommands(commands);
 
+  // var oauthURL = 'https://api.put.io/v2/oauth2/authenticate' +
+  //   '?client_id=2801' +
+  //   '&response_type=token' +
+  //   '&redirect_uri=http://localhost:7000/';
+  //
+
+  var hash = (function(a) {
+      if (a == "") return {};
+      var b = {};
+      for (var i = 0; i < a.length; ++i)
+      {
+          var p=a[i].split('=', 2);
+          if (p.length == 1)
+              b[p[0]] = "";
+          else
+              b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+      }
+      return b;
+  })(window.location.hash.substr(1).split('&'));
+
+    xhr({url: 'https://api.put.io/v2/files/list?parent_id=0&oauth_token=' + hash.access_token , method: 'get'}).then(function (data) {
+      JSON.parse(data).files.forEach(function(file){
+        logCommand(file.name);
+      });
+    });
+
   annyang.start();
 })();
